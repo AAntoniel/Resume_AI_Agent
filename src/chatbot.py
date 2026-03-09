90e4399a47bc45c2d09be46d99ad597e956921c4
Antoniel K. Stefaniak <antonielstefaniak@gmail.com>
2026-03-09 15:59:48 -0300

    feat: adding llm to read the contexts

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,9 +1,14 @@
 from dotenv import load_dotenv
 import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_chroma import Chroma
 
+from datetime import datetime
+
 load_dotenv()
+
+os.environ["GOOGLE_API_KEY"] = os.getenv("GEMINI_API_KEY")
 
 emb = GoogleGenerativeAIEmbeddings(model='models/gemini-embedding-001')
 
@@ -18,14 +23,55 @@ def retrieve(quest):
         k=2
     )
 
-    return results
+
+    # Results retorna uma lista de objetos, mas o modelo não le objetos, e sim textos. Abaixo, extrai-se apenas os textos.  Passa os metadados, 
+    # Como os títulos e junta com o conteúdo para dar o contexto completo para a IA
+    retrieve_texts = []
+
+    for doc in results:
+        title = " > ".join(doc.metadata.values())
+        complete_content = f"[{title}]\n{doc.page_content}"
+
+        retrieve_texts.append(complete_content)
+
+    retrieve_context = "\n\n".join(retrieve_texts)
+
+    return retrieve_context
+
+
+model = ChatGoogleGenerativeAI(
+    model = "gemini-2.5-flash",
+    temperature = 0.2,
+)
+
+today = datetime.now().strftime("%B de %Y")
+
+template = ChatPromptTemplate([
+    ("system", """Você é o assistente virtual do Antoniel. Responda as perguntas dos rcrutadores de forma profissional e simpátca, usando apenas o contexto 
+                  fornecido abaixo.
+     
+                  INFORMAÇÃO TEMPORAL IMPORTANTE: A data de hoje é {today}. Compare essa data com as presentes nos contextos para utilizar o tempo
+                  verbal correto.
+     
+                  CONTEXTO DO CURRÍCULO: {context}. 
+     
+                  Se a resposta não existir no contexto, diga que não sabe."""),
+    ("human", "{user_input}")
+])
 
 if __name__ == "__main__":
-    ask = "Onde ele fez mestrado?"
+    ask = "Onde Antoniel fez mestrado e qual a sua conquista?"
 
-    ans = retrieve(ask)
+    context = retrieve(ask)
 
-    for i in ans:
-        print(i.metadata)
-        print(i.page_content)
-        print("="*20)
+    prompt_value = template.invoke({
+        "context": context,
+        "user_input": ask,
+        "today": today
+    })
+
+    response = model.invoke(prompt_value)
+
+    print(response)
+    print("="*20)
+    print(response.content)
